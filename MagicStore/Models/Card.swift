a022e9a68328f925cd45980925918d149b101e03
Rachel Schneebaum <reschneebaum@gmail.com>
2023-10-03 17:48:52 -0400

    initial UI + models + project setup

---
+++ MagicStore/Models/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  MagicStore
+//
+//  Created by Rachel Schneebaum on 10/3/23.
+//
+
+import Foundation
---
+++ MagicStore/Models/Mana.swift
@@ -0,0 +1,8 @@
+//
+//  Mana.swift
+//  MagicStore
+//
+//  Created by Rachel Schneebaum on 10/3/23.
+//
+
+import Foundation
